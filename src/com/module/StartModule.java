894e451df86aa44f40d0f73aba2d672d39c92dbd
exisign <exisign777@gamil.com>
2020-12-01 15:12:31 +0900

    init project

---
+++ src/com/module/StartModule.java
@@ -0,0 +1,15 @@
+package com.module;
+
+import com.property.PropReader;
+
+public class StartModule {
+	
+	private void readProperties() {
+		PropReader props = new PropReader();
+	}
+
+	public static void main(String[] args) {
+	
+	}
+
+}
